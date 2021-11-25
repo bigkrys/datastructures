bd1bf0c89328877bc6894f110dbe23e395fa6b5b
bigkrys <krysliang@163.com>
2021-11-25 18:19:01 +0800

    add bad bm

--- string.js
+++ string.js
@@ -11,22 +11,21 @@
  * 如果查找到了就返回成功（i当前值）否则返回-1
  */
  function searchBF(string,target){
+    if(target == '') return 0
     let i = 0,flag = false;
-    for(i = 0;i<string.length;){
+    for(i = 0;i<string.length;i++){
         let j = 0;
         while(j<target.length){
-            if(string[i] == target[j]){
-                i++;
-                j++;
+            if(string[i+j] == target[j]){
                 flag = true;
+                j++;
                 if(j==target.length){
-                    return i-j;
+                    return i;
                 }
                 if(i == string.length){
                     return -1;
                 }
             }else{
-                i = i+1;//从下一个字符串长度开始重新计算
                 flag = false;
                 break;
             }
@@ -83,30 +82,29 @@ function isSearchWord(word,target){
  *  https://leetcode-cn.com/problems/implement-strstr/
  */
 
-    var strStr = function(haystack, needle) {
-        if(needle.length==0) return 0;
-        let i = 0;
-        for(i=0;i<haystack.length;){
-            let j = 0;
-            while(j<needle.length){
-                if(haystack[i] == needle[j]){
-                    i++;
-                    j++;
-                    if(j == needle.length){
-                        return i-j;
-                    }
-                    if(i == haystack.length){
-                        return -1;
-                    }
-                }else{
-                    i++;
-                    break;
+ var strStr = function(string, target) {
+    if(target.length == 0) return 0;
+    let i = 0,flag = false;
+    for(i = 0;i<string.length;i++){
+        let j = 0;
+        while(j<target.length){
+            if(string[i+j] == target[j]){
+                flag = true;
+                j++;
+                if(j==target.length){
+                    return i;
                 }
+                if(i == string.length){
+                    return -1;
+                }
+            }else{
+                flag = false;
+                break;
             }
         }
-        return -1;
-    };
-
+    }
+    return -1;
+};
 
 /**
  * RK算法
@@ -186,5 +184,75 @@ function hash(string,miArray,alphabet){
 
 /**
  * BM算法
+ * 也是一个一个的进行对比，只是当模式串与主串进行匹配的过程中，当模式串与主串的某个字符不匹配的时候，
+ * 跳过这个不匹配的情况，将模式串往后多滑动几位。
+ * BM算法包括 坏字符规则和好后缀规则。
  * 
+ * 1、坏字符规则：
+ * BM算法的匹配规则是按照模式串的下标，从大到小倒着匹配，也就是从右到左进行匹配。
+ * 从模式串的末尾往前倒着匹配，当发现某个字符（主串中的字符）不匹配的时候，就把这个不匹配的字符叫做“坏字符”。
+ * 然后拿“坏字符”在模式串中进行查找，发现模式串中不存在这个“坏字符”，于是直接把模式串往后滑动模式串的长度位。
+ * 然后再从模式串的末尾字符开始比较。
+ * 
+ * 当发生不匹配的时候，就把“坏字符”对应模式串中的字符下标几位si，
+ * 如果坏字符在模式串中存在，就把坏字符在模式串中的下标记为xi。
+ * 如果不存在就把xi=-1.
+ * 那么模式串往后移动的位数就等于si-xi.
+ * 
+ * 如果当坏字符在模式串中多处出现，那么xi的下标，选择最靠后那一个，这样就不会让模式串滑动太多，导致前面的没有匹配上。
+ * 
+ * 2、好后缀规则
+ * 当从后往前进行匹配的时候，发现后面的都已经匹配了，但是前面有字符不匹配。
+ * 就把这些已经匹配过的字符叫做好后缀（主串中的字符），记为u。
+ * 然后拿u在模式串中进行查找，如果找到了另一个和u相同的子串v，就把模式串滑动到子串v与主串中u对齐的位置。
+ * 如果在模式串中找不到另一个等于u的子串，
+ * 
+ * a.如果一步步的往后移动模式串的时候，主串中的u与模式串有重合，那肯定就无法完全匹配
+ * （为什么得出这样的结论呢？）
+ * 
+ * b.但是当模式串滑动到前缀与主串中u的后缀有部分重合的时候，并且重合的部分相等的时候，就有可能会存在完全匹配的情况。
+ * 
+ * 所以，针对这种情况，我们不仅要看好后缀在模式串中是否有另一个匹配的子串，我们还要考察好后缀的后缀子串汇总，是否存在跟模式串的前缀子串匹配的。
+ * 比如abc的后缀子串是c,bc。abc的前缀子串是a,ab。
+ * 从好后缀的后缀子串中，找出一个最长的并且跟模式串的前缀子串匹配的，假设是v，然后将模式串移动到v的位置中。
+ * 
+ * 
+ * 
+ * 
+ * 先分别计算好坏字符和好后缀的滑动次数，然后取两个数中最大的，作为模式串往后滑动的位数。
  */
+
+/**
+ * 坏字符规则的实现
+ * 思路：使用散列表来存储模式串中各个字符在模式串中的下标。
+ * 首先使用一个256大小的数组记录每个字符的位置，下标对应字符的accll码，而存储的值就是字符在模式串中的位置
+ */
+function generateHashBC(target){
+    //根据模式串的字符 生成一个散列表
+    let hashArray = new Array(256).fill(-1);
+    for(let i = 0;i<target.length;i++){
+        let ascii = target[i].charCodeAt();
+        hashArray[ascii] = i;
+    }
+    return hashArray;
+}
+function badBM(string,target){
+    let hashBC = generateHashBC(target);//生成模式串的各个哈希表
+    let i = 0,n = string.length;m = target.length;
+    while(i<=n-m){
+        let j;
+        for(j = m-1;j>=0;j--){
+            //从后往前开始匹配
+            if(string[i+j] != target[j]){
+                //遇到不匹配的 退出此处循环
+                break;
+            }
+        }
+        if(j<0){
+            return i;//匹配成功了。
+        }
+        let index = string[i+j].charCodeAt()
+        i = i + (j - hashBC[index]);
+    }
+    return -1;
+}
